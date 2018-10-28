4695eacca28fd067fbc147b48dde0d6b639adbb6
cleliacort <38819355+cleliacort@users.noreply.github.com>
2018-10-28 18:29:06 +0100

    grammar_correction

--- Profile_HMM.sh
+++ Profile_HMM.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-#DESCRIPTION: This script compute automatycally an Profile_HMM and compute the confusion matrix, ACC and MCC for 10 different E_value
-#At starting poitn you have to pass only your customize table of your training set.
-#Then is enough to following step by step the interactive guide
+#DESCRIPTION: This script compute automatycally a Profile_HMM and compute the confusion matrix, ACC and MCC for 10 different E_value
+#To start, you have to pass your customize table of data for the training.
+#Then is enough to following step by step the interactive guide.
 
 #USAGE: ./Profile_HMM.sh [customize_table_of_your training_set]
 #like this >>> ./Profile_HMM.sh tabularResults_wild.csv
 
-#ATTENTION1: to run it you need to download all the file with the same repository organization
-#ATTENTION2: for now you need to replace your working directory in all of the shell scripts that you can find
-#into the scripts directory.
+#ATTENTION1: to run it you need to download all the file and leave them in the repository organization
+#ATTENTION2: you need to replace your working directory in the different shell scripts(have to be adjusted).
 
 #TUTORIAL: to run and see how it work, you can use as input files those uploaded into this folder
 #YOU WILL FIND: 
@@ -42,7 +41,6 @@ mkdir output
 
 #create a list of ids for the structural alignment
 echo -e "I'm creating a list of ids that you can use to make the structural alignment..\n"
-#awk -F " " '{print $1":"$2"}' $output/repr_resolution_wild_95.txt
 awk -F " " '{print $1":"$2}' $output/repr_resolution_wild_95.txt > $output/efold_repr_res_wild_95.txt
 
 echo -e "Pass the previous output file called EFOLD_REPR_WILD_95.TXT to the online MUSCLE to retrieve the multiple structural alignment..\n"
@@ -56,12 +54,12 @@ mkdir model
 model="$HOME/Desktop/LAB1_part2/real_project/resolution_wildtype/github_script/model"
 hmmbuild $model/kunizt.hmm $input/$str_ali
 echo ""
-#cheack it
+#check it
 echo -e "I'm quickly check the model, running a profile search aginst the training set...\n"
 hmmsearch -o $output/hmmsearch_prova.txt $model/kunizt.hmm $output/format_repr_res_wild_95.fasta
 
-#modified the two positive and negative testing set
-#running the hmmsearch against both sets
+#modify the two positive and negative testing set
+#run the hmmsearch against both sets
 #create a file that contain all the values for the confusion matrix
 echo -e "Choose two testing set:one with proteins having the Kunitz domain (read the correlated paper to understand how do it) and the
 the other with proteins that do not have it...\n"
@@ -74,7 +72,7 @@ read test_neg
 echo ""
 ./scripts/pos_neg.sh $test_pos $test_neg $folder
 
-#computed all the values for the confusion matrix
+#compute all the values for the confusion matrix
 echo -e "I'm computing all the values to bufor ten different E-value threshold "
 ./scripts/confusion_matrix.sh $output/rescaled_POS_FRW.txt $output/rescaled_NEG.txt
 
